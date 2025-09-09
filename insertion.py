7b127f1f8a06b591533af0d3d53da28041db1604
vaibhavknight18 <vaibhavganigerv@gmail.com>
2025-09-09 16:19:54 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insertion_sort(A):
+        for i in range(1,len(A)):
+            for j in range(i-1,0,-1):
+                if A[j] > A[j+1]:
+                    A[j],A[j+1] = A[j+1] , A[j]
+                else:
+                    break
+        print("After Sorting !!")
+        print(A)
+if __name__ == "__main__":
+    A = [5,3,7,1]
+    print("Before Sorting !!")
+    print(A)
+    obj = Solution()
+    Solution.insertion_sort(A)
