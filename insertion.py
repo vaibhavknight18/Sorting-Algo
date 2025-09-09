fd0c4b7032fd321518809b1749259d740288a6af
vaibhavknight18 <vaibhavganigerv@gmail.com>
2025-09-09 16:34:47 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -1,7 +1,7 @@
 class Solution:
     def insertion_sort(A):
         for i in range(1,len(A)):
-            for j in range(i-1,0,-1):
+            for j in range(i-1,-1,-1):
                 if A[j] > A[j+1]:
                     A[j],A[j+1] = A[j+1] , A[j]
                 else:
